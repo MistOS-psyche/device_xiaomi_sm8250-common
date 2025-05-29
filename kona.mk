e442b58754823bb65105a2cd7267a9e1417cf467
Rocky7842 <eric.rocky7842@gmail.com>
2025-05-29 12:47:07 +0800

    sm8250-common: Bring back OMX video decoder for MiuiCamera
    * Needed by 960fps slow motion.
    
    Change-Id: Ic97e2c79ddf8cb6189df3d3819f03d3cad7063bd

--- kona.mk
+++ kona.mk
@@ -302,7 +302,9 @@ endif
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
+    media_codecs_kona.xml \
     media_codecs_performance_c2.xml \
+    media_codecs_performance_kona.xml \
     video_system_specs.json
 
 $(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,true)
@@ -325,7 +327,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.nxp.mifare.xml
 
 # OMX
-TARGET_SUPPORTS_OMX_SERVICE := false
+PRODUCT_PACKAGES += \
+    libOmxCore \
+    libOmxVdec \
+    libstagefrighthw
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
