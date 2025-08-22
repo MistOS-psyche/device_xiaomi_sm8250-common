1717a9e8416b65bf71b1184c12d0c271b6cb78fb
Verevka <verevka86@gmail.com>
2025-08-22 16:17:15 +0300

    fixup! sm8250-common: Build Lineage Health HAL

--- kona.mk
+++ kona.mk
@@ -273,7 +273,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/input_suspend)
 
 # Logging
 SPAMMY_LOG_TAGS := \
