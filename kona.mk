a94a0d49d894bb72afc1b6b8a514be19f91a48ed
luk1337 <priv.luk@gmail.com>
2025-09-03 14:30:44 +0300

    sm8250-common: Migrate to AIDL camera provider
    Signed-off-by: Verevka <verevka86@gmail.com>

--- kona.mk
+++ kona.mk
@@ -130,8 +130,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider-service.lineage
 
 PRODUCT_PACKAGES += \
     libutils.vendor \
