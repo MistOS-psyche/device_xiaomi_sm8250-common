dc53384e0455b2ba0fb858bafea0be05f851b953
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-10-30 01:39:31 +0100

    sm8250-common: libinit: Add brackets
    Change-Id: I1d70aeaf0972726d832c144f729067f96b2cb0c6

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2021-2025 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -44,9 +44,11 @@ void set_variant_props(const variant_info& variant) {
         set_ro_build_prop("fingerprint", variant.build_fingerprint);
         property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
 
-        property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
+        property_override("ro.build.description",
+                          fingerprint_to_description(variant.build_fingerprint));
     }
 
-    if (variant.nfc)
+    if (variant.nfc) {
         property_override(SKU_PROP, "nfc");
+    }
 }
